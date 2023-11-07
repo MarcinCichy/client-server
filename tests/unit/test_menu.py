d952ea66e1be3a030275f4cabde1b702ef88fda4
MarcinCichy <marcin-cichy@wp.pl>
2023-11-07 01:11:27 +0100

    Next tests implementation for menu.py module (test_menu - works  quite OK, needs fix)

--- tests/unit/test_menu.py
+++ tests/unit/test_menu.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import patch
+# from unittest.mock import patch
 import server_package.server_response as server_response
 from server_package.menu import CommandHandler
 from server_package.server_user_state import ServerUserState
@@ -8,33 +8,25 @@ from server_package.server_user_state import ServerUserState
 class TestMenu(unittest.TestCase):
     def setUp(self):
         self.logged_in_user_data = ServerUserState()
-        self.logged_in_user_data.username = 'username'
-        self.logged_in_user_data.permissions = 'admin'
+        self.logged_in_user_data.logged_in_username = 'username'
+        self.logged_in_user_data.logged_in_permissions = 'user'
         self.command_handler = CommandHandler(self.logged_in_user_data)
 
     def test_use_command_with_valid_user_command(self):
-        # Przygotowanie danych wejściowych i oczekiwanych wyników
-        entrance_command = {'username': 'help'}  # Zakładamy, że oczekiwany klucz to 'command'
+        entrance_command = {'username': 'help'}
         expected_result = server_response.USER_HELP_DICT
         expected_result_with_additional_info = {'line1': 'line', 'Commands for All': ''}
         expected_result_with_additional_info.update(expected_result)
 
-        # Ustawienie oczekiwanego zachowania metody help
         self.command_handler.sys_utils.help.return_value = expected_result_with_additional_info
 
-        # Wywołanie testowanej metody
         result = self.command_handler.use_command(entrance_command)
 
-        # Aserty
         self.assertEqual(result, expected_result_with_additional_info)
 
-        # Tutaj sprawdzamy, czy pomoc była wywołana, ale nie ma mockowania, więc:
-        # - Jeżeli sys_utils jest zewnętrzną zależnością, powinna być zmockowana
-        # - Jeżeli sys_utils.help nie przyjmuje argumentów, to nie sprawdzamy z jakimi została wywołana
-        # - Jeżeli sys_utils.help przyjmuje argumenty, to powinniśmy to sprawdzić
+        # self.command_handler.sys_utils.help.assert_called_with('user', 'basic')
+        # self.command_handler.sys_utils.help.assert_called()
 
-        # Uwaga: Nie jestem pewien, co dokładnie ma robić testowana metoda use_command,
-        # więc nie mogę dokładnie określić, jak powinna być użyta sys_utils.help.
 
     if __name__ == "__main__":
         unittest.main(verbosity=2)
