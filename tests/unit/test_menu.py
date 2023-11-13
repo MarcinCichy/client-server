12a210be3b4b52552c1ed5f96b9dd79dd1da47ad
MarcinCichy <marcin-cichy@wp.pl>
2023-11-13 06:26:26 +0100

    Next tests implementation for menu.py module. Added unknown permissions Next commands should be checked

--- tests/unit/test_menu.py
+++ tests/unit/test_menu.py
@@ -10,7 +10,8 @@ class TestMenu(unittest.TestCase):
     def test_help_command_with_different_user_permissions(self, mock_sys_utils):
         help_output_user = "Expected Help Output for User"
         help_output_admin = "Expected Help Output for Admin"
-        mock_sys_utils.return_value.help.side_effect = lambda perm: help_output_user if perm == 'user' else help_output_admin
+        help_output_unknown = "Expected Help Output for Exception"
+        mock_sys_utils.return_value.help.side_effect = lambda perm: help_output_user if perm == 'user' else (help_output_admin if perm== 'admin' else help_output_unknown)
 
         user_data = ServerUserState()
         user_data.logged_in_username = 'username'
@@ -19,6 +20,7 @@ class TestMenu(unittest.TestCase):
 
         entrance_command = {'username': 'help'}
         user_result = user_command_handler.use_command(entrance_command)
+        print(user_result)
 
         mock_sys_utils.return_value.help.assert_called_with('user')
         self.assertEqual(user_result, help_output_user)
@@ -33,6 +35,17 @@ class TestMenu(unittest.TestCase):
 
         mock_sys_utils.return_value.help.assert_called_with('admin')
         self.assertEqual(admin_result, help_output_admin)
+
+        unknown_data = ServerUserState()
+        unknown_data.logged_in_username = 'adminusername'
+        unknown_data.logged_in_permissions = 'unknown'
+        unknown_command_handler = CommandHandler(unknown_data)
+
+        entrance_command = {'unknownusername': 'help'}
+        unknown_result = unknown_command_handler.use_command(entrance_command)
+
+        mock_sys_utils.return_value.help.assert_called_with('unknown')
+        self.assertEqual(unknown_result, help_output_unknown)
 
 
 if __name__ == "__main__":
