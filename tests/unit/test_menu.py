b3d08b84e32dc2af773c57595cf984b8947ca4f7
MarcinCichy <marcin-cichy@wp.pl>
2023-11-14 06:20:38 +0100

    Next tests implementation for menu.py module. Added test for stop command Next commands should be checked

--- tests/unit/test_menu.py
+++ tests/unit/test_menu.py
@@ -2,16 +2,16 @@ import unittest
 from unittest.mock import patch
 from server_package.menu import CommandHandler
 from server_package.server_user_state import ServerUserState
+import server_package.server_response as server_response
 
 
 class TestMenu(unittest.TestCase):
-
     @patch('server_package.menu.SystemUtilities')
     def test_help_command_with_different_user_permissions(self, mock_sys_utils):
         help_output_user = "Expected Help Output for User"
         help_output_admin = "Expected Help Output for Admin"
         help_output_unknown = "Expected Help Output for Exception"
-        mock_sys_utils.return_value.help.side_effect = lambda perm: help_output_user if perm == 'user' else (help_output_admin if perm== 'admin' else help_output_unknown)
+        mock_sys_utils.return_value.help.side_effect = lambda perm: help_output_user if perm == 'user' else (help_output_admin if perm == 'admin' else help_output_unknown)
 
         user_data = ServerUserState()
         user_data.logged_in_username = 'username'
@@ -20,7 +20,6 @@ class TestMenu(unittest.TestCase):
 
         entrance_command = {'username': 'help'}
         user_result = user_command_handler.use_command(entrance_command)
-        print(user_result)
 
         mock_sys_utils.return_value.help.assert_called_with('user')
         self.assertEqual(user_result, help_output_user)
@@ -37,7 +36,7 @@ class TestMenu(unittest.TestCase):
         self.assertEqual(admin_result, help_output_admin)
 
         unknown_data = ServerUserState()
-        unknown_data.logged_in_username = 'adminusername'
+        unknown_data.logged_in_username = 'unknownusername'
         unknown_data.logged_in_permissions = 'unknown'
         unknown_command_handler = CommandHandler(unknown_data)
 
@@ -46,6 +45,29 @@ class TestMenu(unittest.TestCase):
 
         mock_sys_utils.return_value.help.assert_called_with('unknown')
         self.assertEqual(unknown_result, help_output_unknown)
+
+    @patch('server_package.menu.SystemUtilities')
+    def test_admin_commands_stop_with_different_user_permissions(self, mock_sys_utils):
+        expected_stop_output_admin = {'Connection': 'close'}
+        expected_stop_output_user = {"Error": "Command unavailable. No permissions"}
+        mock_sys_utils.return_value.stop.return_value = expected_stop_output_admin
+
+        admin_data = ServerUserState()
+        admin_data.logged_in_username = 'adminusername'
+        admin_data.logged_in_permissions = 'admin'
+        admin_command_handler = CommandHandler(admin_data)
+
+        admin_result = admin_command_handler.use_command({'command': 'stop'})
+        self.assertEqual(admin_result, expected_stop_output_admin)
+
+        user_data = ServerUserState()
+        user_data.logged_in_username = 'username'
+        user_data.logged_in_permissions = 'user'
+        user_command_handler = CommandHandler(user_data)
+
+        user_result = user_command_handler.use_command({'command': 'stop'})
+        self.assertEqual(user_result, expected_stop_output_user)
+
 
 
 if __name__ == "__main__":
