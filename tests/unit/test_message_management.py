dbbb391effa319d05d8b7c5a28d7261321126533
MarcinCichy <marcin-cichy@wp.pl>
2023-11-17 06:39:56 +0100

    Next tests implementation for menu.py module. Added test for message_management module Next commands should be checked.

--- tests/unit/test_message_management.py
+++ tests/unit/test_message_management.py
@@ -2,7 +2,7 @@ import unittest
 from unittest.mock import MagicMock
 from server_package.message_management import MessageManagement
 import server_package.server_response as server_response
-
+import server_package.server_data as server_data
 
 
 class TestMessageManagement(unittest.TestCase):
@@ -11,8 +11,20 @@ class TestMessageManagement(unittest.TestCase):
         self.message_management = MessageManagement(self.database_support_mock)
 
     def test_new_message_valid_data(self):
-        self.database_support_mock.get_messages.return_value = {'messages': {}}
-        result = self.message_management.new_message(['sender', 'date', {'recipient': 'username'}, 'content'])
+        self.database_support_mock.get_messages.return_value = {
+            'messages': {
+                'username': []
+            }
+        }
+        # server_data.MAX_MSG_IN_INBOX = 10
+
+        sender = 'sendername'
+        date = 'YYYY-MM-DD'
+        recipient = 'recipient'
+        content = 'MSG CONTENT'
+        data = [sender, date, recipient, content]
+
+        result = self.message_management.new_message(data)
         self.assertEqual(result, server_response.MESSAGE_WAS_SENT)
 
     def test_new_message_invalid_data(self):
@@ -25,7 +37,17 @@ class TestMessageManagement(unittest.TestCase):
         self.assertEqual(result, {"msg": {}})
 
     def test_msg_list_with_messages(self):
-        self.database_support_mock.get_messages.return_value = {'messages': {'username': {'1': 'message1'}}}
+        self.database_support_mock.get_messages.return_value = {
+            'messages': {
+                'username': {
+                    '1': {
+                        'sender': 'sendername',
+                        'date': 'YYYY-MM-DD',
+                        'content': 'MSG CONTENT'
+                    }
+                }
+            }
+        }
         result = self.message_management.msg_list('username')
         self.assertNotEqual(result, {"msg": {}})
 
