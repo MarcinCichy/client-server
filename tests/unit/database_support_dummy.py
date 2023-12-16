a9f50dd5beb241e5214dd3e726884cdc365a71d9
MarcinCichy <marcin-cichy@wp.pl>
2023-12-16 17:28:31 +0100

    Test message_management method without using Mock and MagicMock

---
+++ tests/unit/database_support_dummy.py
@@ -0,0 +1,55 @@
+class DatabaseSupportDummy:
+    def __init__(self):
+        self.users_data = {"users":
+            {
+                "RECIPIENT": {
+                    "password": "pass",
+                    "permissions": "user",
+                    "status": "active",
+                    "activation_date": "2023-04-06"
+                },
+                "other_user": {
+                    "password": "pass2",
+                    "permissions": "admin",
+                    "status": "banned",
+                    "activation_date": "2023-04-06"
+                }
+            },
+            "logged_users": [
+                "username_invalid",
+                "logged_username"
+    ]
+        }
+        self.messages_data = {'messages':
+            {
+                'RECIPIENT': {
+                    "1": {'sender': 'user2', 'date': '2023-01-01', 'content': 'Hello'},
+                    "2": {'sender': 'user3', 'date': '2023-01-02', 'content': 'Hi'},
+                    "3": {'test_message_3'}
+                },
+                'FULL_INBOX_USER': {
+                    "1": {'sender': 'user2', 'date': '2023-01-01', 'content': 'Hello'},
+                    "2": {'sender': 'user3', 'date': '2023-01-02', 'content': 'Hi'},
+                    "3": {'sender': 'user3', 'date': '2023-01-02', 'content': 'Hi'},
+                    "4": {'sender': 'user3', 'date': '2023-01-02', 'content': 'Hi'},
+                    "5": {'sender': 'user3', 'date': '2023-01-02', 'content': 'Hi'},
+                },
+                'NO_MSGS_USER': {
+            }
+            }
+        }
+
+    def get_user(self):
+        return self.users_data
+
+    def save_user(self, data):
+        self.users_data = data
+
+    def get_messages(self):
+        return self.messages_data
+
+    def save_messages(self, data):
+        self.messages_data = data
+
+    # Metody read_db_json i save_db_json nie są potrzebne, ponieważ operujemy bezpośrednio na danych w pamięci
+
--- tests/unit/test_message_management.py
+++ tests/unit/test_message_management.py
@@ -1,128 +1,72 @@
 import unittest
 from server_package.message_management import MessageManagement
 import server_package.server_response as server_response
-from server_package.database_support import DatabaseSupport
+from database_support_dummy import DatabaseSupportDummy
+from server_package import server_data as server_data
 
 
 class TestMessageManagement(unittest.TestCase):
     def setUp(self):
-        # self.database_support_dummy = {'messages':
-        #     {
-        #         'RECIPIENT': {
-        #             "1": {'sender': 'user2', 'date': '2023-01-01', 'content': 'Hello'},
-        #             "2": {'sender': 'user3', 'date': '2023-01-02', 'content': 'Hi'}
-        #         }
-        #     }
-        # }
-        self.database_support = DatabaseSupport()
-        self.msg_mgmt = MessageManagement(self.database_support)
+        self.db_support_dummy = DatabaseSupportDummy()
+        self.msg_mgmt = MessageManagement(self.db_support_dummy)
 
     def test_new_message_valid_data(self):
         sender = {'sender': 'sendername'}
         date = {'date': 'YYYY-MM-DD'}
-        recipient = {'recipient': 'marcin'}
+        recipient = {'recipient': 'RECIPIENT'}
         content = {'content': 'MSG CONTENT'}
         data = [sender, date, recipient, content]
-        # data = ['sender', 'date', {'recipient': 'username'}, 'content']
         response = self.msg_mgmt.new_message(data)
         self.assertEqual(response, server_response.MESSAGE_WAS_SENT)
 
     def test_new_message_invalid_data(self):
-        data = None  # Symulacja nieprawidłowych danych
+        data = None
         response = self.msg_mgmt.new_message(data)
         self.assertEqual(response, server_response.E_INVALID_DATA)
 
-    # Możesz dodać więcej testów dla różnych scenariuszy
+    def test_new_message_full_inbox(self):
+        # full_inbox = {str(i): 'message' for i in range(1, server_data.MAX_MSG_IN_INBOX + 1)}
+        full_inbox = {}
+        for i in range(1, server_data.MAX_MSG_IN_INBOX + 1):
+            full_inbox[str(i)] = 'message'
+
+        sender = {'sender': 'sendername'}
+        date = {'date': 'YYYY-MM-DD'}
+        recipient = {'recipient': 'FULL_INBOX_USER'}
+        content = {'content': 'MSG CONTENT'}
+        data = [sender, date, recipient, content]
+
+        result = self.msg_mgmt.new_message(data)
+        self.assertEqual(result, server_response.E_RECIPIENT_INBOX_IS_FULL)
+
+    def test_msg_list_no_messages(self):
+        result = self.msg_mgmt.msg_list('NO_MSGS_USER')
+        self.assertEqual(result, {"msg": {}})
+
+    def test_msg_list_with_messages(self):
+        result = self.msg_mgmt.msg_list('RECIPIENT')
+        self.assertNotEqual(result, {"msg": {}})  # ?????
+
+    def test_msg_del_existing_message(self):
+        result = self.msg_mgmt.msg_del({'RECIPIENT': '1'})
+        self.assertEqual(result, server_response.MESSAGE_WAD_DELETED)
+
+    def test_msg_del_non_existing_message(self):
+        result = self.msg_mgmt.msg_del({'RECIPIENT': '7'})
+        self.assertEqual(result, server_response.E_MESSAGE_NOT_FOUND)
+
+    def test_msg_show_existing(self):
+        result = self.msg_mgmt.msg_show({'RECIPIENT': '3'})
+        self.assertEqual(result, {"Message to show": {'test_message_3'}})
+
+    def test_msg_show_non_existing(self):
+        result = self.msg_mgmt.msg_show({'RECIPIENT': '4'})
+        self.assertEqual(result, server_response.E_MESSAGE_NOT_FOUND)
+
+    def test_msg_count(self):
+        result = self.msg_mgmt.msg_count('RECIPIENT')
+        self.assertEqual(result, {"msg-inbox-count": 3})
 
 
 if __name__ == '__main__':
-    unittest.main()
-
-# import unittest
-# from unittest.mock import MagicMock
-# from server_package.message_management import MessageManagement
-# from server_package import server_data as server_data
-# import server_package.server_response as server_response
-#
-#
-# class TestMessageManagement(unittest.TestCase):
-#     def setUp(self):
-#         self.database_support_mock = MagicMock()
-#         self.message_management = MessageManagement(self.database_support_mock)
-#         self.database_support_mock.get_messages.return_value = {'messages':
-#             {
-#                 'RECIPIENT': {
-#                     "1": {'sender': 'user2', 'date': '2023-01-01', 'content': 'Hello'},
-#                     "2": {'sender': 'user3', 'date': '2023-01-02', 'content': 'Hi'}
-#                 }
-#             }
-#         }
-#
-#     def test_new_message_valid_data(self):
-#         sender = {'sender': 'sendername'}
-#         date = {'date': 'YYYY-MM-DD'}
-#         recipient = {'recipient': 'RECIPIENT'}
-#         content = {'content': 'MSG CONTENT'}
-#         data = [sender, date, recipient, content]
-#
-#         result = self.message_management.new_message(data)
-#         self.assertEqual(result, server_response.MESSAGE_WAS_SENT)
-#
-#     def test_new_message_invalid_data(self):
-#         result = self.message_management.new_message(None)
-#         self.assertEqual(result, server_response.E_INVALID_DATA)
-#
-#     def test_new_message_full_inbox(self):
-#         # full_inbox = {str(i): 'message' for i in range(1, server_data.MAX_MSG_IN_INBOX + 1)}
-#         full_inbox = {}
-#         for i in range(1, server_data.MAX_MSG_IN_INBOX + 1):
-#             full_inbox[str(i)] = 'message'
-#         self.database_support_mock.get_messages.return_value = {'messages': {'FULL_INBOX_USER': full_inbox}}
-#
-#         sender = {'sender': 'sendername'}
-#         date = {'date': 'YYYY-MM-DD'}
-#         recipient = {'recipient': 'FULL_INBOX_USER'}
-#         content = {'content': 'MSG CONTENT'}
-#         data = [sender, date, recipient, content]
-#
-#         result = self.message_management.new_message(data)
-#         self.assertEqual(result, server_response.E_RECIPIENT_INBOX_IS_FULL)
-#
-#     def test_msg_list_no_messages(self):
-#         self.database_support_mock.get_messages.return_value = {'messages': {'username': {}}}
-#         result = self.message_management.msg_list('username')
-#         self.assertEqual(result, {"msg": {}})
-#
-#     def test_msg_list_with_messages(self):
-#         result = self.message_management.msg_list('RECIPIENT')
-#         self.assertNotEqual(result, {"msg": {}})
-#
-#     def test_msg_del_existing_message(self):
-#         self.database_support_mock.get_messages.return_value = {'messages': {'username': {'1': 'message1'}}}
-#         result = self.message_management.msg_del({'username': '1'})
-#         self.assertEqual(result, server_response.MESSAGE_WAD_DELETED)
-#
-#     def test_msg_del_non_existing_message(self):
-#         self.database_support_mock.get_messages.return_value = {'messages': {'username': {}}}
-#         result = self.message_management.msg_del({'username': '1'})
-#         self.assertEqual(result, server_response.E_MESSAGE_NOT_FOUND)
-#
-#     def test_msg_show_existing(self):
-#         self.database_support_mock.get_messages.return_value = {'messages': {'username': {'1': 'message1'}}}
-#         result = self.message_management.msg_show({'username': '1'})
-#         self.assertEqual(result, {"Message to show": 'message1'})
-#
-#     def test_msg_show_non_existing(self):
-#         self.database_support_mock.get_messages.return_value = {'messages': {'username': {}}}
-#         result = self.message_management.msg_show({'username': '1'})
-#         self.assertEqual(result, server_response.E_MESSAGE_NOT_FOUND)
-#
-#     def test_msg_count(self):
-#         self.database_support_mock.get_messages.return_value = {
-#             'messages': {'username': {'1': 'message1', '2': 'message2'}}}
-#         result = self.message_management.msg_count('username')
-#         self.assertEqual(result, {"msg-inbox-count": 2})
-#
-#
-# if __name__ == "__main__":
-#     unittest.main(verbosity=2)
+    unittest.main(verbosity=2)
