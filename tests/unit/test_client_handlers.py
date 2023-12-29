da710d7f5ba54333a2a66d14e8ca2b6b40843564
MarcinCichy <marcin-cichy@wp.pl>
2023-12-30 00:45:59 +0100

    Added Test for Client handlers - prepare command to send to server

---
+++ tests/unit/test_client_handlers.py
@@ -0,0 +1,31 @@
+import unittest
+from client_package.windows.handlers import Handlers
+
+
+class TestClientHandlers(unittest.TestCase):
+    def test_prepare_command_empty(self):
+        assert Handlers.prepare_command("user", "") == {"user": None}
+
+    def test_prepare_command_without_data(self):
+        command = "help"
+        expected = {"user": "help"}
+        assert Handlers.prepare_command("user", command) == expected
+
+    def test_prepare_command_user_del(self):
+        command = "user-del to_delete_user"
+        expected = {"user": {"user-del": "to_delete_user"}}
+        assert Handlers.prepare_command("user", command) == expected
+
+    def test_prepare_command_user_del_without_name_of_user_to_delete(self):
+        command = "user-del"
+        expected = {"user": {"user-del": None}}
+        assert Handlers.prepare_command("user", command) == expected
+
+    def test_prepare_command_user_stat(self):
+        command = "user-stat anybody banned"
+        expected = {"user": {"user-stat": {"anybody": "banned"}}}
+        assert Handlers.prepare_command("user", command) == expected
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
